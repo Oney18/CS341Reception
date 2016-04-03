164455e2448bc01236fb3eccf4356b0ef2e17cfc
NalaniKai <megan.nalani@gmail.com>
2016-04-03 14:27:58 -0700

    Hide contact and time input fields after submitting party info

--- ReservationGUI/ReservationGUI/ReservationForm.cs
+++ ReservationGUI/ReservationGUI/ReservationForm.cs
@@ -52,13 +52,17 @@ namespace ReservationGUI
                 //check if reservation is in restaurant operating hours and the phone number is a 7 digit number
                 if(resHour < 21 && resHour > 10 && resMin > -1 && resMin < 61 && contactNum.Length == 7 && int.TryParse(contactNum, out check))
                 {
-                    wait.addReservation(guestNumTextBox.Text, nameTextBox.Text, requestsTextBox.Text, contactNum, resHour, resMin);
+                    wait.addReservation(guestNumTextBox.Text, nameTextBox.Text, requestsTextBox.Text, contactNum, resHour, resMin);                   
                 }
                 else
                 {
                     MessageBox.Show("The contact number must be 7 digits and the reservation " +
                         "must be between 11:00 and 21:00. This reservation was not made, try again.");
-                }                
+                }
+
+                //hide time and contact input fields
+                hideTime();
+                hideContact();
             }
             else if (takeOutRadioButton.Checked)
             {
@@ -70,8 +74,25 @@ namespace ReservationGUI
                 else
                 {
                     MessageBox.Show("The contact number must be 7 digits, try again.");
-                }                
+                }
+
+                //hide contact input field
+                hideContact();
             }
+        }
+
+        private void hideTime()
+        {
+            reservationTimeLabel.Visible = false;
+            timeDescriptionLabel.Visible = false;
+            reservationHourTextBox.Visible = false;
+            reservationMinTextBox.Visible = false;
+        }
+
+        private void hideContact()
+        {
+            contactLabel.Visible = false;
+            contactTextBox.Visible = false;
         }
 
         private void guestNumTextBox_TextChanged(object sender, EventArgs e)
@@ -86,6 +107,7 @@ namespace ReservationGUI
         {
             //show reservation boxes to get time and contact for reservation
             reservationTimeLabel.Visible = true;
+            timeDescriptionLabel.Visible = true;
             reservationHourTextBox.Visible = true;
             reservationMinTextBox.Visible = true;
             contactLabel.Visible = true;
