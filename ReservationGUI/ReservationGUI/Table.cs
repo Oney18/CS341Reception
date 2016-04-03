c2c990cee78dd7e7b964e2b33d1aa424cc7395fb
oney18 <oney18@up.edu>
2016-04-03 12:16:53 -0700

    Added waitlist and table classes

---
+++ ReservationGUI/ReservationGUI/Table.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReservationGUI
+{
+    class Table
+    {
+        private bool inUse;
+        private int peopleSeated;
+        private bool ableToBeSeated;
+        private Party partySeated;
+        private int tableNum;
+        static public int SIZE_OF_TABLE = 4;
+
+        public Table(int num)
+        {
+            this.tableNum = num;
+            inUse = false;
+            ableToBeSeated = true;
+            partySeated = null;
+        }
+
+        public void seat(Party p)
+        {
+            if (ableToBeSeated)
+            {
+                this.partySeated = p;
+                p.seat(this.tableNum);
+                inUse = true;
+                ableToBeSeated = false;
+            }
+        }
+
+        public Party leave()
+        {
+            Party temp = partySeated;
+            partySeated = null;
+            inUse = false;
+            return temp;
+        }
+
+        public void clean()
+        {
+            if (!inUse) ableToBeSeated = true;
+        }
+
+        public bool getAbleToBeSeated()
+        {
+            return ableToBeSeated;
+        }
+
+
+    }
+}
---
+++ ReservationGUI/ReservationGUI/Waitlist.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReservationGUI
+{
+    class Waitlist
+    {
+        private Table[] tableList;
+        private LinkedList<Party> reservationsPresent;
+        private LinkedList<Party> walkIns;
+        private ArrayList reservations;
+        private ArrayList pastReservations;
+
+        public Waitlist(int num)
+        {
+            reservationsPresent = new LinkedList<Party>();
+            walkIns = new LinkedList<Party>();
+            reservations = new ArrayList();
+            pastReservations = new ArrayList();
+            tableList = new Table[num];
+            for (int i = 0; i < num; i++)
+            {
+                tableList[i] = new Table(i);
+            }
+        }
+
+        public void addReservation(int partySize, String name, String specialReq, int phoneNum, int month, int day, int hour, int minute)
+        {
+            DateTime reservationTime = new DateTime(DateTime.Now.Year, month, day, hour, minute, 0);
+            reservations.Add(new Party(partySize, name, specialReq, phoneNum, reservationTime));
+        }
+
+        public void checkIn(String name)
+        {
+            Party partyToCheck = null;
+
+            foreach (Party res in reservations)
+            {
+                if (res.getName().Equals(name))
+                {
+                    partyToCheck = res;
+                    break;
+                }
+            }
+
+            if (partyToCheck != null)
+            {
+                if (Int32.Parse(partyToCheck.getResTime().Subtract(DateTime.Now).ToString().Substring(3, 2)) <= 15)
+                {
+                    reservationsPresent.AddLast(partyToCheck);
+                    reservations.Remove(partyToCheck);
+                }
+                else Console.WriteLine("Party {0} cannot be checked in; more than 15 minutes out.", name);
+            }
+            else
+            {
+                Console.WriteLine("Could not find party under name {0}.", name);
+            }
+
+        }
+
+        public void addWalkIn(int partySize, String name, String specialReq, int phoneNum)
+        {
+            walkIns.AddLast(new Party(partySize, name, specialReq, phoneNum));
+        }
+
+        public Party getNextParty()
+        {
+            if (reservationsPresent.Count() > 0)
+            {
+                Party temp = reservationsPresent.First();
+                reservationsPresent.RemoveFirst();
+                return temp;
+            }
+            else if (walkIns.Count() > 0)
+            {
+                Party temp = walkIns.First();
+                walkIns.RemoveFirst();
+                return temp;
+            }
+            return null;
+        }
+
+        public void seatNextParty(int tableNum)
+        {
+            if (tableList[tableNum].getAbleToBeSeated())
+            {
+                Party temp = getNextParty();
+                tableList[tableNum].seat(temp);
+            }
+        }
+
+
+
+        public void ToManagement()
+        {
+            using (System.IO.StreamWriter file =
+            new System.IO.StreamWriter(@"C:\ReceptionFiles\ReceptionManagement.txt"))
+            {
+                foreach (Party party in pastReservations)
+                {
+                    file.WriteLine(party.managementOutput());
+                }
+            }
+        }
+
+
+    }
+}
